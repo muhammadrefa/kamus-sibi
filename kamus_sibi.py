711bf16f68446836b06a2a7d62ef4215513f7d7a
Muhammad Refa Utama Putra <muhammadrefa@aol.com>
2018-07-04 00:05:57 +0700

    Menambah fitur deskripsi dan perbaikan
    - Fitur deskripsi dengan membaca berkas txt
    - Tombol 'Tampilkan' dimatikan secara asali
    - Status bar lebih informatif
    - Penampilan laman galat jika tidak vide tidak ditemukan
    - Penambahan kategori

--- kamus_sibi.py
+++ kamus_sibi.py
@@ -17,14 +17,26 @@ class firstGUI(QtGui.QMainWindow, Ui_MainWindow):
 
         kategori = []
         self.butir = []
+        self.deskripsi = {}
 
         kategori.append("Alfabet")
-        self.butir.append(["A", "B", "C", "D", "E", "F", "G"])
+        self.butir.append(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                           "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
 
         kategori.append("Keluarga")
         self.butir.append(
             ["Ayah", "Ibu", "Anak", "Adik", "Kakak", "Kakek", "Nenek", "Bibi (Tante)", "Paman (Om)", "Suami", "Istri",
              "Cucu", "Bayi"])
+
+        kategori.append("Hari")
+        kategori.append("Kata")
+        kategori.append("Rasa")
+        kategori.append("Sifat")
+        kategori.append("Bulan")
+        kategori.append("Profesi")
+        kategori.append("Ucapan")
+        kategori.append("Angka")
+        kategori.append("Hewan")
 
         self.list_kategori.addItems(kategori)
         self.list_kategori.currentItemChanged.connect(self.kategori_dipilih)
@@ -35,44 +47,65 @@ class firstGUI(QtGui.QMainWindow, Ui_MainWindow):
         self.statusbar.showMessage("Siap! Pilih kategori dan butir, lalu klik 'Tampilkan'")
 
     def kategori_dipilih(self):
+        self.btn_tampil.setEnabled(False)
         kategori = self.list_kategori.currentItem().text()
         self.webview.load("./data/mohon.html")
         self.label_butir.setText(kategori + " -> ")
         self.label_deskripsi.setText("")
-        self.statusbar.showMessage("Siap! Pilih kategori dan butir, lalu klik 'Tampilkan' untuk menampilkan video")
+        self.statusbar.showMessage("Siap! Pilih butir untuk ditampilkan")
         self.list_item.clear()
         try:
             self.list_item.addItems(self.butir[self.list_kategori.currentRow()])
         except:
             pass
+        self.deskripsi = self.ambilDeskripsi(kategori)
 
     def item_dipilih(self):
         kategori = self.list_kategori.currentItem().text()
         item = self.list_item.currentItem().text()
-        self.webview.load("./data/mohon.html")
         self.label_butir.setText(kategori + " -> " + item)
-        self.label_deskripsi.setText("Deskripsi untuk " + kategori + " -> " + item)
-        self.statusbar.showMessage("Siap! Pilih kategori dan butir, lalu klik 'Tampilkan' untuk menampilkan video")
+        try:
+            self.label_deskripsi.setText(self.deskripsi[item])
+            self.statusbar.showMessage("Klik 'Tampilkan' untuk menampilkan video")
+        except KeyError:
+            self.label_deskripsi.setText("Tidak ada deskripsi")
+            self.statusbar.showMessage("Deskripsi untuk " + kategori + " -> " + item + " tidak dapat dimuat!")
+        if os.path.isfile("data/sibi/" + kategori + "/" + item + ".mp4"):
+            self.webview.load("./data/mohon.html")
+            self.btn_tampil.setEnabled(True)
+        else:
+            self.webview.load("./data/galat.html")
+            self.btn_tampil.setEnabled(False)
 
     def tampil(self):
         try:
             kategori = self.list_kategori.currentItem().text()
             item = self.list_item.currentItem().text()
             # print(self.list_kategori.currentItem().text() + " -> " + self.list_item.currentItem().text())
-            print(kategori + " -> " + item)
-            self.label_butir.setText(kategori + " -> " + item)
-            berkas_video = "./data/video_sibi/" + kategori + "/" + item + ".mp4"
+            # print(kategori + " -> " + item)
+            berkas_video = "./data/sibi/" + kategori + "/" + item + ".mp4"
             if os.path.isfile(berkas_video):
                 self.webview.load(berkas_video)
                 self.statusbar.showMessage("Menampilkan video untuk " + kategori + " -> " + item)
             else:
                 self.statusbar.showMessage("Galat! Video tidak ditemukan")
         except:
-            print("GALAT")
+            pass
+
+    def ambilDeskripsi(self, kategori):
+        deskripsi = {}
+        try:
+            with open("data/sibi/" + kategori + ".txt", "r") as f:
+                for line in f:
+                    split = line.split("=")
+                    deskripsi[split[0]] = split[1]
+        except FileNotFoundError:
+            self.statusbar.showMessage("Deskripsi untuk kategori " + kategori + " tidak dapat dimuat!")
+        return deskripsi
 
     def funcTentang(self):
         appPos = self.pos()
-        print("x -> " + str(appPos.x()) + " y -> " + str(appPos.y()))
+        # print("x -> " + str(appPos.x()) + " y -> " + str(appPos.y()))
         self.tentang = windowTentang()
         self.tentang.move(appPos.x()+(self.tentang.width()/2), appPos.y()+(self.tentang.height()/2))
         self.tentang.show()
@@ -83,7 +116,7 @@ class windowTentang(QtGui.QWidget, Ui_FormTentang):
         self.setupUi(self)
 
 if __name__ == "__main__":
-    print(os.getcwd())
+    # print(os.getcwd())
     # app = QtWidgets.QApplication(sys.argv)
     # MainWindow = QtWidgets.QMainWindow()
 
